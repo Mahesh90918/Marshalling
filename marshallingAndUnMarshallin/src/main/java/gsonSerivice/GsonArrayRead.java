4d2188ec3a86cd464917d636c6ad81fd59e177e6
user <user@mahesh>
2022-09-15 11:02:58 +0530

    Arraylist gson

--- marshallingAndUnMarshallin/src/main/java/gsonSerivice/GsonArrayRead.java
+++ marshallingAndUnMarshallin/src/main/java/gsonSerivice/GsonArrayRead.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
@@ -15,10 +16,19 @@ public class GsonArrayRead {
 	public static void main(String[] args) throws JsonSyntaxException, JsonIOException, IOException {
 		Gson gson = new Gson();
 		FileReader file = new FileReader("src/main/java/gsonSerivice/GsonArray.json");
-		JsonElement a = gson.fromJson(file, JsonElement.class);
+		// 1.way Read
+		JsonElement json = gson.fromJson(file, JsonElement.class);
+		System.out.println(json);
+
+		// 2.way Read
+		// line bye Line Reading
+		ArrayList a = gson.fromJson(file, ArrayList.class);
 		// String s=gson.toJson(a);
 		System.out.println(a);
-		
-		
+		ArrayList al = new ArrayList(a);
+		Iterator i = al.iterator();
+		while (i.hasNext()) {
+			System.out.println(i.next());
+		}
 	}
 }
